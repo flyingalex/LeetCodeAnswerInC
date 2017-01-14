2f75186237bc365d48f42aa6662d94a9ebc80788
flyingalex <hulinalex@163.com>
2017-01-14 13:39:44 +0800

    fix fizz_buzz bug

--- fizz_buzz.c
+++ fizz_buzz.c
@@ -1,28 +1,58 @@
 #include <stdlib.h>
-#include <stdio.h>  
+#include <stdio.h>
+#include <math.h>
 
-char** fizzBuzz(int n, int* returnSize) {
+char** fizzBuzz(int);
+int count(int);
 
-    char ** fizzBuzz = [][];
-    const char *FIZZ = "Fizz";
-    const char *BIZZ = "Bizz";
-    const char *FIZZ_BUZZ = "FizzBizz";
-    int i;
-    for (i = 0; i < n; i++)
+// test
+int main(int argc, char **argv)
+{
+    int n = 50;
+    char ** result;
+    result = fizzBuzz(n);
+    for(int i = 0; i < n; i++)
+      printf("%s\n", result[i]);
+    return 0;
+}
+
+char** fizzBuzz(int n) {
+    
+    char ** fizzBuzz;
+    fizzBuzz = malloc(sizeof(char *)*n);
+    char *FIZZ = "Fizz";
+    char *BIZZ = "Buzz";
+    char *FIZZ_BUZZ = "FizzBuzz";
+    for (int i = 1; i <= n; i++)
     {
+        int index = i -1;
         if (i%15 == 0) {
-         fizzBuzz[i] = malloc(sizeof *FIZZ_BUZZ;
-         fizzBuzz[i] = *FIZZ_BUZZ;
+            fizzBuzz[index] = malloc(sizeof(char)*9);
+            fizzBuzz[index] = FIZZ_BUZZ;
         } else if (i%3 == 0) {
-          fizzBuzz[i] = malloc(sizeof *FIZZ);
-          fizzBuzz[i] = *FIZZ;
+            fizzBuzz[index] = malloc(sizeof(char)*5);
+            fizzBuzz[index] = FIZZ;
         } else if (i%5 == 0) {
-          fizzBuzz[i] = malloc(sizeof *BIZZ);
-          fizzBuzz[i] = *BIZZ;
+            fizzBuzz[index] = malloc(sizeof(char)*5);
+            fizzBuzz[index] = BIZZ;
         } else {
-          fizzBuzz[i] = malloc(sizeof int);
-          fizzBuzz[i] = i;
+            fizzBuzz[index] = malloc((count(i)+1)*sizeof(char));
+            sprintf(fizzBuzz[index], "%d", i);
         }
     }
     return fizzBuzz;
 }
+
+// integer length
+int count(int n)
+{
+    int count = 0;
+    while(n != 0)
+    {
+        // n = n/10
+        n /= 10;
+        ++count;
+    }
+    return count;
+}
+
